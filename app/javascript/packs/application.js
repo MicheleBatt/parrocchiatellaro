8edb6342b7d6be258b7757887ded6e71457579e6
MicheleBatt <michelebattistelli92@gmail.com>
2022-04-17 19:19:25 +0200

    fix sui file application.js e backoffice.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,6 @@
 var componentRequireContext = require.context("application/components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+window.React = global.React = require("react");
+window.ReactDOM = global.ReactDOM = require("react-dom");
+window.$ = $;
